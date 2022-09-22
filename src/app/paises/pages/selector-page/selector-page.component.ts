051efe94810c2ab185de244ba94058a4c0fb433b
JeanCornelio <jcornelo@isf.do>
2022-09-22 14:23:29 -0400

    Selecctores casi terminado

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { switchMap } from 'rxjs';
-import { paisSmall } from '../../interfaces/paises.interfaces';
-import { PaisesService} from '../../services/paises.service';
+import { switchMap, tap } from 'rxjs';
+import { Pais, PaisSmall } from '../../interfaces/paises.interfaces';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -11,33 +11,65 @@ import { PaisesService} from '../../services/paises.service';
   ]
 })
 export class SelectorPageComponent implements OnInit {
-  
+
   frm: FormGroup = this.fb.group({
-    region:['', Validators.required],
-    pais:['',Validators.required]
+    region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required]
   })
 
   constructor(private fb: FormBuilder,
     private paisesServices: PaisesService) { }
-    
+
   //llenar selector
-  regiones: string[]=[];
-  paises: paisSmall[]=[];
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  // fronteras: string[]=[];
+  fronteras: PaisSmall[] =[];
+  cargando: boolean = false;
 
   ngOnInit(): void {
-  this.regiones = this.paisesServices.regiones
+    this.regiones = this.paisesServices.regiones
 
-  // Cuando Cambia el Valor
-  this.frm.get('region')?.valueChanges
-  .pipe(
-    switchMap((region)=> this.paisesServices.getPaisesporRegion(region))
-  ).subscribe(resp=>{
-    this.paises = resp
-    console.log(this.paises)
-  })
-}
+    // Cuando Cambia el valor del continente
+    this.frm.get('region')?.valueChanges
+      .pipe(
+        //Esto lanza un efecto secundario al ver que el valor cambio
+        tap(()=>{
+          this.paises = []
+          this.frm.get('pais')?.reset('');
+          this.cargando = true
+        }),
+        switchMap((region) => this.paisesServices.getPaisesPorRegion(region))
+      ).subscribe(paises => {
+        this.paises = paises
+        this.cargando = false
+      })
+      
+      
+    //Cuando cambia el valor del Pais
+    
+    this.frm.get('pais')?.valueChanges.pipe(
+         //Esto lanza un efecto secundario al ver que el valor cambio
+         tap(()=>{
+          this.fronteras = []
+          this.frm.get('frontera')?.reset('');
+          this.cargando = true
+        }),
+      switchMap(codigo => this.paisesServices.getPaisPorCodigo(codigo))
+    ).subscribe( pais  =>{
+     if(pais){
+      this.fronteras = pais;
+      this.cargando = false
+      /* if(pais[0].borders){
+        this.fronteras = pais[0].borders;
+       
+       } */
+     }
+    })
+  }
 
-  guardar(){
+  guardar() {
     console.log(this.frm.value)
   }
 
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { paisSmall } from '../interfaces/paises.interfaces';
-
+import { PaisSmall, Pais } from '../interfaces/paises.interfaces';
 @Injectable({
   providedIn: 'root'
 })
@@ -18,9 +17,41 @@ export class PaisesService {
   constructor(private http: HttpClient) { }
 
 
-  getPaisesporRegion(region:string):Observable<paisSmall[]>{
+  getPaisesPorRegion(region:string):Observable<PaisSmall[]>{
     const url:string =`${this.baseUrl}/region/${region}?fields=name,cca3`
-    return this.http.get<paisSmall[]>(url)
+    return this.http.get<PaisSmall[]>(url)
     
   }
+ 
+  getPaisPorCodigo(codigo:string ):Observable<Pais[]  | null >{
+    if(!codigo){
+      return of(null)
+    }
+
+    const url:string =`${this.baseUrl}/alpha/${codigo}`
+    return this.http.get<Pais[]>(url)
+  }
+
+   
+  getPaisPorCodigoSmall(codigo:string ):Observable<PaisSmall>{
+    const url:string =`${this.baseUrl}/alpha/${codigo}?fields=name,cca3`;
+    return this.http.get<PaisSmall>(url)
+  }
+
+  getPaisesPorCodigos(borders:string[]):Observable<PaisSmall[]>{
+    if(!borders){
+      return of([]);
+    }
+    const peticiones: Observable<PaisSmall> []= [];
+
+    borders.forEach(  codigo =>{
+      const peticion = this.getPaisPorCodigoSmall( codigo );
+      peticiones.push(peticion)
+    })
+
+    return combineLatest(peticiones);
+  }
+
+
+
 }
